e097d7c457598546a0bf39b3205c5c6fcc841e27
Adjeks Tega <oadjeks@gmail.com>
2022-10-14 18:14:11 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: return
+ */
+
+int main(void){
+
+	int num1, num2;
+	/*ASCII code for 0*/
+	num 1 = 48;
+
+	while (num1 < 58)
+	{
+	
+		putchar(num1);
+		putchar(num2);
+		num2++;
+		putchar(',');
+		putchar(' ');
+	}
+	
+	num1++;
+}
+putchar('\n');
+return (0);
+
+}

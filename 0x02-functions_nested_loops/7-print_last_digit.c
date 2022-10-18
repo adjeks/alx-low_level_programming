285c1da05b506bec7b48a71788b1d6a5f7c835b6
Adjeks Tega <oadjeks@gmail.com>
2022-10-18 13:44:42 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,19 @@
 
 /**
  * print_last_digit - prints the last digit
- * @n: the number
- * Return: value of the last digit
- */
+ * @n: int we get last digit from
+ * Return: return
+*/
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int z;
 
-	if (ld < 0)
-		ld *= -1;
+	if (n < 0)
+		n = -n;
+	z = n % 10;
+	if (z < 0)
+		z = -z;
+	_putchar(z + '0');
 
-	_putchar(ld + '0');
-
-	return (0);
+	return (z);
 }

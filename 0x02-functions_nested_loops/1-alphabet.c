aa740f15a649a4bd3e1d3e475a75e909af31a7ec
Adjeks Tega <oadjeks@gmail.com>
2022-10-18 12:33:31 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char xy;
+
+	for (xy = 'a'; xy <= 'z'; xy++)
+		_putchar(xy);
+
+	_putchar('\n');
+}
